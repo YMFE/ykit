dc8ad5fbc984e6c3594e6b3e81dffb65560df51a
yuhao.ju <heiwu805@hotmail.com>
2016-07-08 11:56:07 +0800

    feat(server): add logger

--- src/commands/server.js
+++ src/commands/server.js
@@ -4,6 +4,7 @@ let connect = require('connect'),
     http = require('http'),
     serveStatic = require('serve-static'),
     serveIndex = require('serve-index'),
+    moment = require('moment'),
     webpackDevMiddleware = require("webpack-dev-middleware");
 
 let Manager = require('../modules/manager.js');
@@ -18,7 +19,7 @@ exports.setOptions = (optimist) => {
     optimist.alias('p', 'port');
     optimist.describe('p', '端口');
     optimist.alias('m', 'middlewares');
-    optimist.describe('m', '加载项目中间件')
+    optimist.describe('m', '加载项目中间件');
 };
 
 exports.run = (options) => {
@@ -42,6 +43,57 @@ exports.run = (options) => {
         })
     }
 
+    // logger
+    app.use(function() {
+        const dateFormat = 'YY.MM.DD HH:mm:ss';
+        const parse = function(req, res, format) {
+            var status = (function() {
+        		switch (true) {
+        			case 500 <= res.statusCode:
+        				return '\x1b[31m';
+        			case 400 <= res.statusCode:
+        				return '\x1b[33m';
+        			case 300 <= res.statusCode:
+        				return '\x1b[36m';
+        			case 200 <= res.statusCode:
+        				return '\x1b[32m';
+        		}
+        	})();
+
+            let contentLength = res._contentLength || '';
+            if(contentLength){
+                contentLength = contentLength > 1024
+                                ? (contentLength / 1024).toFixed(2) + 'kb '
+                                : contentLength + 'b ';
+            }
+
+        	format = format.replace(/%date/g, "\x1b[90m" + '[' + (moment().format(dateFormat)) + ']' + "\x1b[0m");
+        	format = format.replace(/%method/g, "\x1b[35m" + (req.method.toUpperCase()) + "\x1b[0m");
+        	format = format.replace(/%url/g, "\x1b[90m" + options.cwd + decodeURI(req.originalUrl) + "\x1b[0m");
+        	format = format.replace(/%status/g, "" + status + res.statusCode + "\x1b[0m");
+            format = format.replace(/%route/g, "\x1b[90m" + (req.route ? req.route.path + ' ' : '\x1b[31m') + "\x1b[0m");
+        	format = format.replace(/%contentLength/g, "\x1b[90m" + contentLength + '\x1b[31m' + "\x1b[0m");
+        	format = format.replace(/%(date|time)/g, "\x1b[90m" + (new Date - req._startTime) + "ms\x1b[0m");
+        	return format;
+        };
+
+        return function(req, res, next) {
+        	var end;
+        	req._startTime = new Date;
+        	end = res.end;
+        	res.end = function(chunk, encoding) {
+        		res.end = end;
+        		res.end(chunk, encoding);
+                const format = '%date %status %method %url (%route%contentLength%time)'
+        		const message = parse(req, res, format);
+        		return process.nextTick(function() {
+        			return info(message);
+        		});
+        	};
+        	return next();
+        };
+    }());
+
     app.use(function(req, res, next) {
         let url = req.url,
             keys = url.split('/');
@@ -58,13 +110,13 @@ exports.run = (options) => {
                 if (project.check()) {
                     let compiler = project.getCompiler();
                     middleware = middlewareCache[projectName] = webpackDevMiddleware(compiler, {
-                        hot: hot
+                        noInfo: true
+                        // hot: hot TODO
                     });
                 } else {
                     next();
                     return;
                 }
-
             }
             req.url = '/' + keys.slice(3).join('/').replace(/(\@[\d\w]+)?\.(js|css)/, '.$2');
             middleware(req, res, next);
@@ -81,4 +133,5 @@ exports.run = (options) => {
     app.use(serveIndex(cwd));
 
     http.createServer(app).listen(port);
+    info('Listening on port ' + port)
 };
