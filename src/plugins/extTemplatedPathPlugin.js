f4578c89ca551b0e0f07d7ac68030eb35a24620d
yuhao.ju <heiwu805@hotmail.com>
2016-07-12 19:46:50 +0800

    feat(ext plguin): 支持js后缀转换

--- src/plugins/extTemplatedPathPlugin.js
+++ src/plugins/extTemplatedPathPlugin.js
@@ -2,16 +2,25 @@
 
 module.exports = {
     apply: (compiler) => {
-        var cssExtNames = compiler.options.entryExtNames.css;
+        const entryExtNames = compiler.options.entryExtNames
+
         compiler.plugin("compilation", function(compilation) {
             compilation.mainTemplate.plugin("asset-path", function(path, data) {
                 let extName = '[ext]';
                 if (data.chunk) {
                     let rawRequest = data.chunk.origins[0].module.rawRequest;
                     extName = sysPath.extname(rawRequest);
-                    if (cssExtNames.indexOf(sysPath.extname(sysPath.basename(rawRequest, '.js'))) > -1) {
+
+                    if (entryExtNames.css.indexOf(sysPath.extname(sysPath.basename(rawRequest, '.js'))) > -1) {
                         extName = '.cache';
                     }
+
+                    // 应用后缀转换规则
+                    Object.keys(entryExtNames).map((targetExtName) => {
+                        if(entryExtNames[targetExtName].indexOf(extName) > -1){
+                            extName = '.' + targetExtName
+                        }
+                    })
                 }
                 return path.replace(/\[ext\]/g, extName);
             });
