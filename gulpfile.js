385748dd76a2f6873efb7aff58876a5b75245c50
yuhao.ju <heiwu805@hotmail.com>
2016-07-29 17:47:41 +0800

    feat(gulp): 第一次启动时也编译

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const watch = require('gulp-watch');
 gulp.task('default', ['compileJS', 'moveConfig']);
 
 gulp.task('compileJS', [], function() {
-	return watch('src/**/*.js', { verbose: true })
+	return watch('src/**/*.js', { verbose: true, ignoreInitial: false })
         .pipe(babel({presets: ['es2015']}))
 		.pipe(gulp.dest('lib'));
 })
 
 gulp.task('moveConfig', [], function() {
-    return watch('src/config/**/*.*', { verbose: true }, () => {
+    return watch('src/config/**/*.*', { verbose: true, ignoreInitial: false }, () => {
         gulp.src('src/config/**/*.*', {base: 'src/'})
             .pipe(gulp.dest('lib'));
     })
