877896cdc8f704d9d2b2b7cd02f8598e2a7f930f
yuhao.ju <heiwu805@hotmail.com>
2016-08-01 14:12:53 +0800

    fix(install): proxy改为使用时提示安装

--- src/commands/server.js
+++ src/commands/server.js
@@ -8,6 +8,7 @@ let connect = require('connect'),
     livereload = require('livereload'),
     connectLivereload = require('connect-livereload'),
     child_process = require('child_process'),
+    inquirer = require('inquirer'),
     webpackDevMiddleware = require("webpack-dev-middleware");
 
 let Manager = require('../modules/manager.js');
@@ -37,143 +38,175 @@ exports.run = (options) => {
         enableLivereload = options.l || options.livereload,
         port = options.p || options.port || 80;
 
-    let middlewareCache = {};
+    let isGoingToStartServer = true
 
-    if (middlewares) {
-        middlewares.split('|').forEach((proName) => {
-            let pro = Manager.getProject(sysPath.join(cwd, proName));
-            if (pro.check() && Array.isArray(pro.middlewares)) {
-                pro.middlewares.forEach((mw) => app.use(mw));
-            }
-        })
+    // 检测预装插件
+    if(proxy){
+        try {
+            require.resolve("@qnpm/jerryproxy-ykit")
+        } catch(e) {
+            var questions = [{
+            	type: 'confirm',
+            	name: 'isInstallingProxy',
+            	message: 'Prxoy plugin not installed yet, wounld you like to install it now?'
+            }];
+
+            inquirer.prompt(questions).then((answers) => {
+                if(answers.isInstallingProxy) {
+                    const installCmd = 'npm i @qnpm/jerryproxy-ykit --registry http://registry.npm.corp.qunar.com/';
+                    try {
+                        log('intalling @qnpm/jerryproxy-ykit ...')
+                        child_process.execSync(installCmd, {cwd: sysPath.resolve(__dirname, '../../')});
+                    } catch (e) {
+                        error(e);
+                    }
+                    log('@qnpm/jerryproxy-ykit successfully installed');
+                }
+            })
+
+            isGoingToStartServer = false
+        }
     }
 
-    // logger
-    app.use((req, res, next) => {
-        const end = res.end;
-    	req._startTime = new Date;
+    if(isGoingToStartServer) {
+        let middlewareCache = {};
 
-    	res.end = (chunk, encoding) => {
-    		res.end = end;
-    		res.end(chunk, encoding);
-            const format = '%date %status %method %url (%route%contentLength%time)'
-    		const message = parse(req, res, format);
-    		return process.nextTick(() => {
-    			return info(message);
-    		});
-    	};
+        if (middlewares) {
+            middlewares.split('|').forEach((proName) => {
+                let pro = Manager.getProject(sysPath.join(cwd, proName));
+                if (pro.check() && Array.isArray(pro.middlewares)) {
+                    pro.middlewares.forEach((mw) => app.use(mw));
+                }
+            })
+        }
 
-        function parse(req, res, format) {
-            const dateFormat = 'YY.MM.DD HH:mm:ss';
-            const status = (function() {
-        		switch (true) {
-        			case 500 <= res.statusCode:
-        				return '\x1b[31m';
-        			case 400 <= res.statusCode:
-        				return '\x1b[33m';
-        			case 300 <= res.statusCode:
-        				return '\x1b[36m';
-        			case 200 <= res.statusCode:
-        				return '\x1b[32m';
-        		}
-        	})();
+        // logger
+        app.use((req, res, next) => {
+            const end = res.end;
+        	req._startTime = new Date;
 
-            let contentLength = res._contentLength || '';
-            if(contentLength){
-                contentLength = contentLength > 1024
-                                ? (contentLength / 1024).toFixed(2) + 'kB '
-                                : contentLength + 'bytes ';
-            }
+        	res.end = (chunk, encoding) => {
+        		res.end = end;
+        		res.end(chunk, encoding);
+                const format = '%date %status %method %url (%route%contentLength%time)'
+        		const message = parse(req, res, format);
+        		return process.nextTick(() => {
+        			return info(message);
+        		});
+        	};
 
-        	format = format.replace(/%date/g, "\x1b[90m" + '[' + (moment().format(dateFormat)) + ']' + "\x1b[0m");
-        	format = format.replace(/%method/g, "\x1b[35m" + (req.method.toUpperCase()) + "\x1b[0m");
-        	format = format.replace(/%url/g, "\x1b[90m" + decodeURI(req.originalUrl) + "\x1b[0m");
-        	format = format.replace(/%status/g, "" + status + res.statusCode + "\x1b[0m");
-            format = format.replace(/%route/g, "\x1b[90m" + (req.route ? req.route.path + ' ' : '\x1b[31m') + "\x1b[0m");
-        	format = format.replace(/%contentLength/g, "\x1b[90m" + contentLength + '\x1b[31m' + "\x1b[0m");
-        	format = format.replace(/%(date|time)/g, "\x1b[90m" + (new Date - req._startTime) + "ms\x1b[0m");
-        	return format;
-        };
+            function parse(req, res, format) {
+                const dateFormat = 'YY.MM.DD HH:mm:ss';
+                const status = (function() {
+            		switch (true) {
+            			case 500 <= res.statusCode:
+            				return '\x1b[31m';
+            			case 400 <= res.statusCode:
+            				return '\x1b[33m';
+            			case 300 <= res.statusCode:
+            				return '\x1b[36m';
+            			case 200 <= res.statusCode:
+            				return '\x1b[32m';
+            		}
+            	})();
 
-    	return next();
-    });
+                let contentLength = res._contentLength || '';
+                if(contentLength){
+                    contentLength = contentLength > 1024
+                                    ? (contentLength / 1024).toFixed(2) + 'kB '
+                                    : contentLength + 'bytes ';
+                }
 
-    app.use((req, res, next) => {
-        let url = req.url,
-            keys = url.split('/');
+            	format = format.replace(/%date/g, "\x1b[90m" + '[' + (moment().format(dateFormat)) + ']' + "\x1b[0m");
+            	format = format.replace(/%method/g, "\x1b[35m" + (req.method.toUpperCase()) + "\x1b[0m");
+            	format = format.replace(/%url/g, "\x1b[90m" + decodeURI(req.originalUrl) + "\x1b[0m");
+            	format = format.replace(/%status/g, "" + status + res.statusCode + "\x1b[0m");
+                format = format.replace(/%route/g, "\x1b[90m" + (req.route ? req.route.path + ' ' : '\x1b[31m') + "\x1b[0m");
+            	format = format.replace(/%contentLength/g, "\x1b[90m" + contentLength + '\x1b[31m' + "\x1b[0m");
+            	format = format.replace(/%(date|time)/g, "\x1b[90m" + (new Date - req._startTime) + "ms\x1b[0m");
+            	return format;
+            };
 
-        if (keys[2] == 'prd') {
-            let projectName = keys[1],
-                projectCwd = sysPath.join(cwd, projectName),
-                middleware = middlewareCache[projectName];
+        	return next();
+        });
 
-            if (!middleware) {
-                let project = Manager.getProject(projectCwd);
-                if (project.check()) {
-                    let compiler = project.getServerCompiler();
-                    middleware = middlewareCache[projectName] = webpackDevMiddleware(compiler, {quiet: true});
+        app.use((req, res, next) => {
+            let url = req.url,
+                keys = url.split('/');
 
-                    // 输出server运行中 error/warning 信息
-                    compiler.watch({}, function(err, stats) {
-                        const statsInfo = stats.toJson({errorDetails: false}),
-                            logMethods = {
-                                errors: error,
-                                warnings: warn
-                            };
+            if (keys[2] == 'prd') {
+                let projectName = keys[1],
+                    projectCwd = sysPath.join(cwd, projectName),
+                    middleware = middlewareCache[projectName];
 
-                        Object.keys(logMethods).map((typeId) => {
-                            statsInfo[typeId].map((logInfo) => {
-                                logMethods[typeId](logInfo);
+                if (!middleware) {
+                    let project = Manager.getProject(projectCwd);
+                    if (project.check()) {
+                        let compiler = project.getServerCompiler();
+                        middleware = middlewareCache[projectName] = webpackDevMiddleware(compiler, {quiet: true});
+
+                        // 输出server运行中 error/warning 信息
+                        compiler.watch({}, function(err, stats) {
+                            const statsInfo = stats.toJson({errorDetails: false}),
+                                logMethods = {
+                                    errors: error,
+                                    warnings: warn
+                                };
+
+                            Object.keys(logMethods).map((typeId) => {
+                                statsInfo[typeId].map((logInfo) => {
+                                    logMethods[typeId](logInfo);
+                                });
                             });
                         });
-                    });
 
-                } else {
-                    next();
-                    return;
+                    } else {
+                        next();
+                        return;
+                    }
                 }
+                req.url = '/' + keys.slice(3).join('/').replace(/(\@[\d\w]+)?\.(js|css)/, '.$2');
+                middleware(req, res, next);
+            } else {
+                next();
             }
-            req.url = '/' + keys.slice(3).join('/').replace(/(\@[\d\w]+)?\.(js|css)/, '.$2');
-            middleware(req, res, next);
-        } else {
-            next();
+        });
+
+        if(enableLivereload){
+            livereload.createServer().watch(cwd);
+            app.use(connectLivereload({port: 35729}));
         }
-    });
 
-    if(enableLivereload){
-        livereload.createServer().watch(cwd);
-        app.use(connectLivereload({port: 35729}));
-    }
+        app.use(serveStatic(cwd, {
+            redirect: false,
+            index: false
+        }));
 
-    app.use(serveStatic(cwd, {
-        redirect: false,
-        index: false
-    }));
+        app.use(serveIndex(cwd));
 
-    app.use(serveIndex(cwd));
+        const httpServer = http.createServer(app)
+        httpServer.on('error', (e) => {
+            if(e.code === 'EACCES'){
+                warn('权限不足, 请使用sudo执行')
+            } else if(e.code === 'EADDRINUSE'){
+                warn('端口 ' + port + ' 已经被占用, 请关闭占用该端口的程序或者使用其它端口.')
+            }
+            process.exit(1)
+        })
+        httpServer.listen(port, () => {
+            warn('Listening on port ' + port);
+        })
 
-    const httpServer = http.createServer(app)
-    httpServer.on('error', (e) => {
-        if(e.code === 'EACCES'){
-            warn('权限不足, 请使用sudo执行')
-        } else if(e.code === 'EADDRINUSE'){
-            warn('端口 ' + port + ' 已经被占用, 请关闭占用该端口的程序或者使用其它端口.')
+        // 代理
+        if(proxy) {
+            const proxyPath = sysPath.join(require.resolve('@qnpm/jerryproxy-ykit'), '../bin/jerry.js')
+            child_process.fork(proxyPath);
         }
-        process.exit(1)
-    })
-    httpServer.listen(port, () => {
-        warn('Listening on port ' + port);
-    })
 
-    // 代理
-    if(proxy){
-        const proxyPath = sysPath.join(require.resolve('@qnpm/jerryproxy-ykit'), '../bin/jerry.js')
-        child_process.fork(proxyPath);
+        // 权限降级
+        if (process.env['SUDO_UID']) {
+            process.setuid(parseInt(process.env['SUDO_UID']));
+        }
+
     }
-
-    // 权限降级
-    if (process.env['SUDO_UID']) {
-        process.setuid(parseInt(process.env['SUDO_UID']));
-    }
-
 };
