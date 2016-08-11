7cace66d88d0edaf923fe225f547329ad236f240
yuhao.ju <heiwu805@hotmail.com>
2016-08-11 12:15:54 +0800

    release0.0.3rc1

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const watch = require('gulp-watch');
 gulp.task('default', ['compileJS', 'moveConfig']);
 
 gulp.task('compileJS', [], function() {
-	return watch('src/**/*.js', { verbose: true, ignoreInitial: false })
+	return watch('src/**/*.js', { verbose: true, ignoreInitial: true })
         .pipe(babel({presets: ['es2015']}))
 		.pipe(gulp.dest('lib'));
 })
 
 gulp.task('moveConfig', [], function() {
-    return watch('src/config/**/*.*', { verbose: true, ignoreInitial: false }, () => {
+    return watch('src/config/**/*.*', { verbose: true, ignoreInitial: true }, () => {
         gulp.src('src/config/**/*.*', {base: 'src/'})
             .pipe(gulp.dest('lib'));
     })
