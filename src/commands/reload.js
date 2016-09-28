5e36d451fc4aa5896b349e02ad592fa4b0b90790
yuhao.ju <yuhao.ju@qunar.com>
2016-09-28 13:57:39 +0800

    revise(reload): reload命令默认只获取本地插件

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -11,61 +11,66 @@ exports.usage = "重载插件"
 exports.setOptions = (optimist) => {
     optimist.alias('s', 'service')
     optimist.describe('s', '拉取插件服务地址')
+    optimist.alias('g', 'global')
+    optimist.describe('g', 'reload全局插件')
 };
 
 exports.run = (options) => {
     const servicePath = options.s || options.service || 'http://l-uedmobile0.h.dev.cn0.qunar.com:3000/download'
+    const reloadGlobal = options.g || options.global || false
 
-    // 重载全局插件
-    Manager.reloadRC();
+    if(reloadGlobal) {
+        // 重载全局插件
+        Manager.reloadRC();
+    } else {
+        // 检查devDependencies中的ykit-config-{name}
+        const packageJsonContent = JSON.parse(fs.readFileSync('./package.json', {encoding: 'utf-8'}))
+        const ykitModuleReg = /^(@[^\/]+\/)(ykit\-(\w+)\-[\w\-]+)$/
 
-    // 检查devDependencies中的ykit-config-{name}
-    const packageJsonContent = JSON.parse(fs.readFileSync('./package.json', {encoding: 'utf-8'}))
-    const ykitModuleReg = /^(@[^\/]+\/)(ykit\-(\w+)\-[\w\-]+)$/
+        let willInstallPackages = []
 
-    let willInstallPackages = []
+        if(packageJsonContent.devDependencies) {
+            const devDependencies = packageJsonContent.devDependencies
+            Object.keys(devDependencies).map((depName, i) => {
+                if(ykitModuleReg.test(depName) && !requireg.resolve(depName)) {
+                    const version = devDependencies[depName]
+                    const packagePath = './node_modules/@qnpm'
 
-    if(packageJsonContent.devDependencies) {
-        const devDependencies = packageJsonContent.devDependencies
-        Object.keys(devDependencies).map((depName, i) => {
-            if(ykitModuleReg.test(depName) && !requireg.resolve(depName)) {
-                const version = devDependencies[depName]
-                const packagePath = './node_modules/@qnpm'
+                    if(semver.valid(version)) {
+                        // 确保存在@qnpm目录
+                        mkdirp.sync(packagePath)
 
-                if(semver.valid(version)) {
-                    // 确保存在@qnpm目录
-                    mkdirp.sync(packagePath)
+                        // 下载
+                        const tarName = depName + '@' + version + '.tar.gz'
+                        const extractPath = sysPath.join(process.cwd(), './node_modules/', tarName)
+                        const downloadPath = servicePath + '/' + tarName
+                        const downloadStream = request(downloadPath)
+                            .on('response', function(response) {
+                                log(`[${response.statusCode}] downloading ${downloadPath}`)
+                             })
+                            .pipe(fs.createWriteStream(extractPath))
 
-                    // 下载
-                    const tarName = depName + '@' + version + '.tar.gz'
-                    const extractPath = sysPath.join(process.cwd(), './node_modules/', tarName)
-                    const downloadPath = servicePath + '/' + tarName
-                    const downloadStream = request(downloadPath)
-                        .on('response', function(response) {
-                            log(`[${response.statusCode}] downloading ${downloadPath}`)
-                         })
-                        .pipe(fs.createWriteStream(extractPath))
-
-                    // 解压
-                    downloadStream.on('finish', (e) => {
-                        if(e) {
-                            return error(e)
-                        }
-
-                        log('extracting ' + extractPath)
-                        child_process.exec('cd ./node_modules/@qnpm/ && tar -xf ' + extractPath, (e, stdout, errout) => {
+                        // 解压
+                        downloadStream.on('finish', (e) => {
                             if(e) {
                                 return error(e)
                             }
 
-                            success('complete!')
-                            fs.unlinkSync(extractPath)
+                            log('extracting ' + extractPath)
+                            child_process.exec('cd ./node_modules/@qnpm/ && tar -xf ' + extractPath, (e, stdout, errout) => {
+                                if(e) {
+                                    return error(e)
+                                }
+
+                                success('complete!')
+                                fs.unlinkSync(extractPath)
+                            })
                         })
-                    })
-                } else {
-                    error(depName + '版本号必须确定，如0.1.0')
+                    } else {
+                        error(depName + '版本号必须确定，如0.1.0')
+                    }
                 }
-            }
-        })
+            })
+        }
     }
-};
+}
