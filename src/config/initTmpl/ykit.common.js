656c74ea4d3ffb1ca8592e03d77c0e28ee651136
yuhao.ju <yuhao.ju@qunar.com>
2016-10-14 14:13:42 +0800

    revise(ykit.common.js): 修改ykit默认配置文件模板

--- src/config/initTmpl/ykit.common.js
+++ src/config/initTmpl/ykit.common.js
@@ -1,29 +1,25 @@
 exports.config = function() {
-    // 项目资源入口
-    this.setExports([
-        // './scripts/index.js',
-        // './styles/index.css'
-    ]);
+    return {
+        export: [
+            './scripts/index.js',
+            './styles/index.scss'
+        ],
+        modifyWebpackConfig: function(baseConfig) {
+            // 修改 webpack 编译配置
+            // ...
 
-    // 编译环境配置
-    this.setCompiler({
-        context: './src'
-    });
-
-    // sync到开发机配置
-    this.setSync({
-        host : "192.168.237.71",
-        path: "/home/q/www/qunarzz.com/#_name"
-    });
-
-    // 自定义命令
-    this.setCommands([
-        {
+            return baseConfig
+        },
+        sync: {
+            host: "192.168.237.71",
+            path: "/home/q/www/qunarzz.com/#_name"
+        },
+        commands: [{
             name: '#_name_cmd',
             module: {
                 usage: '项目自定义命令',
-                run: function () {}
+                run: function() {}
             }
-        }
-    ]);
+        }]
+    }
 };
