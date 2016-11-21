f36fcf4ea356f6af5317a47be6d9247d0af95fd8
yuhao.ju <yuhao.ju@qunar.com>
2016-11-21 16:09:23 +0800

    chore: 添加 gulp 编译报错

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,23 @@ const watch = require('gulp-watch');
 gulp.task('default', ['compileJS', 'moveConfig']);
 
 gulp.task('compileJS', [], function() {
-	return watch('src/**/*.js', { verbose: true, ignoreInitial: true })
-        .pipe(babel({presets: ['es2015']}))
-		.pipe(gulp.dest('lib'));
+	var babelProcess = babel({presets: ['es2015']})
+    babelProcess.on('error', function(e) {
+        console.log(e);
+        process.exit(1);
+    });
+
+    return watch('src/**/*.js', {
+        verbose: true,
+        ignoreInitial: true
+    }).pipe(babelProcess).pipe(gulp.dest('lib'));
 })
 
 gulp.task('moveConfig', [], function() {
-    return watch('src/config/**/*.*', { verbose: true, ignoreInitial: true }, () => {
-        gulp.src('src/config/**/*.*', {base: 'src/'})
-            .pipe(gulp.dest('lib'));
+    return watch('src/config/**/*.*', {
+        verbose: true,
+        ignoreInitial: true
+    }, () => {
+        gulp.src('src/config/**/*.*', {base: 'src/'}).pipe(gulp.dest('lib'));
     })
 });
