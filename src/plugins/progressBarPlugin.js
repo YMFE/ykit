3e67b01ec187ed58f698f732f73814102db765c4
yuhao.ju <yuhao.ju@qunar.com>
2016-12-06 11:23:13 +0800

    feat(pack): 给出明确 optimize 提示

--- src/plugins/progressBarPlugin.js
+++ src/plugins/progressBarPlugin.js
@@ -13,6 +13,10 @@ function ProgressBarPlugin() {
         if(msg) {
             if(msg !== 'emit') {
                 spinner.text = '[Bundler] ' + msg;
+
+                if(msg.indexOf('optimize') > -1) {
+                    spinner.render();
+                }
             } else {
                 const dateFormat = 'YY.MM.DD HH:mm:ss';
                 spinner.text = '\x1b[90m' + '[' +moment().format(dateFormat) +'] build complete!' + '\x1b[0m';
