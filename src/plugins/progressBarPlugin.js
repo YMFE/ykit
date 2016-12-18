d80c3a0261de04fe91584fdedfe527e8f42cf379
Ellery0924 <383188743@qq.com>
2016-12-19 00:25:18 +0800

    feat(progressBarPlugin.js): 现在ykit server能够在控制台中显示每次build的时间了。

--- src/plugins/progressBarPlugin.js
+++ src/plugins/progressBarPlugin.js
@@ -5,22 +5,26 @@ const moment = require('moment');
 module.exports = new ProgressBarPlugin();
 
 function ProgressBarPlugin() {
-    return new webpack.ProgressPlugin(function(percent, msg) {
-        if(percent === 0) {
+    let startTime = null;
+    let endTime = null;
+    return new webpack.ProgressPlugin(function (percent, msg) {
+        if (percent === 0) {
             spinner.text = 'building...';
             spinner.start();
+            startTime = Date.now();
         }
 
-        if(msg) {
-            if(msg !== 'emit') {
+        if (msg) {
+            if (msg !== 'emit') {
                 spinner.text = '[Bundler] ' + msg;
 
-                if(msg.indexOf('optimize') > -1) {
+                if (msg.indexOf('optimize') > -1) {
                     spinner.render();
                 }
             } else {
+                endTime = Date.now();
                 const dateFormat = 'YY.MM.DD HH:mm:ss';
-                spinner.text = '\x1b[90m' + '[' +moment().format(dateFormat) +'] build complete!' + '\x1b[0m';
+                spinner.text = '\x1b[90m' + '[' + moment().format(dateFormat) + '] build complete in ' + (endTime - startTime) + 'ms.';
                 spinner.stopAndPersist(logSymbols.info);
                 spinner.text = '';
             }
