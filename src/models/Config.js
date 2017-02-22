e907691924706a6369e4ccc024fac7258ae8d58e
yuhao.ju <yuhao.ju@qunar.com>
2017-02-22 18:31:52 +0800

    fix(config): 修复一些情况下 alias 路径设置重复

--- src/models/Config.js
+++ src/models/Config.js
@@ -143,7 +143,10 @@ class Config {
             if (nextConfig.resolve && nextConfig.resolve.alias) {
                 let alias = nextConfig.resolve.alias;
                 Object.keys(alias).map((key) => {
-                    if (key.indexOf('$') !== key.length - 1 && /^\/.+/.test(alias[key])) {
+                    if (key.indexOf('$') !== key.length - 1
+                        && /^\/.+/.test(alias[key])
+                        && alias[key].indexOf(this._config.cwd) === -1
+                    ) {
                         alias[key] = sysPath.join(this._config.cwd, alias[key]);
                     }
                 });
