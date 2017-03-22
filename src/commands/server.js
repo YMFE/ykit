8fc1ccc19971cab86aa1888b837d4eba85f137b0
yuhao.ju <yuhao.ju@qunar.com>
2017-03-22 15:01:10 +0800

    fix(server): modify publicPath replacing to shutdown

--- src/commands/server.js
+++ src/commands/server.js
@@ -199,27 +199,27 @@ exports.run = (options) => {
             return;
         }
 
-        // 修改 publicPath 为当前服务
-        if(project.config.replacingPublicPath !== false) {
-            let localPublicPath = wpConfig.output.local.publicPath;
-            const hostReg = /(http:|https:)?(\/\/)([^\/]+)/i;
-            if(localPublicPath && localPublicPath.match(hostReg).length === 4) {
-                localPublicPath = '/' + UtilPath.normalize(localPublicPath, false);
-                localPublicPath = localPublicPath.replace(hostReg, (matches, httpStr, splitStr, host) => {
-                    httpStr = httpStr || '';
-                    return httpStr + '//' + '127.0.0.1:' + port;
-                });
-
-                wpConfig.output.local.publicPath = localPublicPath;
-            } else {
-                // hot 且 未指定 publicPath 需要手动设置方式 hot.json 404
-                const relativePath = sysPath.relative(projectCwd, wpConfig.output.local.path);
-                wpConfig.output.local.publicPath = `/${projectName}/${relativePath}/`;
-            }
-        }
-
         // hot reload
         if(hot) {
+            // 修改 publicPath 为当前服务
+            if(project.config.replacingPublicPath !== false) {
+                let localPublicPath = wpConfig.output.local.publicPath;
+                const hostReg = /(http:|https:)?(\/\/)([^\/]+)/i;
+                if(localPublicPath && localPublicPath.match(hostReg).length === 4) {
+                    localPublicPath = '/' + UtilPath.normalize(localPublicPath, false);
+                    localPublicPath = localPublicPath.replace(hostReg, (matches, httpStr, splitStr, host) => {
+                        httpStr = httpStr || '';
+                        return httpStr + '//' + '127.0.0.1:' + port;
+                    });
+
+                    wpConfig.output.local.publicPath = localPublicPath;
+                } else {
+                    // hot 且 未指定 publicPath 需要手动设置方式 hot.json 404
+                    const relativePath = sysPath.relative(projectCwd, wpConfig.output.local.path);
+                    wpConfig.output.local.publicPath = `/${projectName}/${relativePath}/`;
+                }
+            }
+
             wpConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
             if(!usingHotServer) {
