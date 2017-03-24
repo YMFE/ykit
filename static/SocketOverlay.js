9dba33ec82c74aeade69b4bbaef065fbdd4bb30f
yuhao.ju <yuhao.ju@qunar.com>
2017-03-24 13:17:26 +0800

    chore: modify overlay layout

--- static/SocketOverlay.js
+++ static/SocketOverlay.js
@@ -2,7 +2,7 @@
     // add overlay
     var overlay = document.createElement('div');
     overlay.style.cssText = 'display: none; position:absolute; left: 0; right: 0; top: 0; bottom: 0; padding: 15px;'
-                          + 'opacity: 0.85; z-index: 99999999; line-height: 20px; word-wrap: break-word; overflow: auto;'
+                          + 'opacity: 0.85; z-index: 9999999; line-height: 20px; word-wrap: break-word; overflow: auto;'
                           + 'font-size:14px; background-color:#000; color:#aaa;';
     overlay.style.fontFamily = 'Lucida Console, Lucida Sans Typewriter, monaco, Bitstream Vera Sans Mono, monospace';
     document.body.appendChild(overlay);
@@ -58,8 +58,29 @@
                 });
             });
 
+            // 添加按钮
+            var btnStyle = 'position: absolute; z-index:99999999; bottom: 70px; right: 15px; width: 100px; opacity: 0.9;'
+                          + 'padding: 10px 0; font-size: 14px; border: 1px solid #ccc; border-radius: 3px;'
+                          + 'background-color: transparent; color: #eee; cursor: pointer;';
+
+            var btnReload = document.createElement('button');
+            btnReload.innerText = 'Reload';
+            btnReload.style.cssText = btnStyle + 'bottom: 15px; background-color: #09c;';
+            btnReload.onclick = function(){location.reload()}
+
+            var btnDismiss = document.createElement('button');
+            btnDismiss.innerText = 'Dismiss';
+            btnDismiss.style.cssText = btnStyle + 'bottom: 65px; background-color: transparent;';
+            btnDismiss.onclick = function(){
+                overlay.style.display = 'none';
+                btnReload.style.display = 'none';
+                btnDismiss.style.display = 'none';
+            }
+
             if(fragment.children.length > 0) {
                 overlay.style.display = 'block';
+                document.body.appendChild(btnDismiss);
+                document.body.appendChild(btnReload);
             } else {
                 // 提示进行 reload
                 var doneLabelEle = document.createElement('span');
@@ -72,24 +93,6 @@
                 doneTextEle.innerText = 'Compiled successfully. Please reload the page.';
                 fragment.appendChild(doneTextEle);
             }
-
-            // 添加按钮
-            var btnStyle = 'position: absolute; bottom: 70px; right: 15px; width: 100px; opacity: 0.9;'
-                          + 'padding: 10px 0; font-size: 14px; border: 1px solid #ccc; border-radius: 3px;'
-                          + 'background-color: transparent; color: #eee; cursor: pointer;';
-
-            var btnDismiss = document.createElement('button');
-            btnDismiss.innerText = 'Dismiss';
-            btnDismiss.style.cssText = btnStyle + 'bottom: 65px; background-color: transparent;';
-            btnDismiss.onclick = function(){overlay.style.display = 'none';}
-
-            var btnReload = document.createElement('button');
-            btnReload.innerText = 'Reload';
-            btnReload.style.cssText = btnStyle + 'bottom: 15px; background-color: #09c;';
-            btnReload.onclick = function(){location.reload()}
-
-            fragment.appendChild(btnReload);
-            fragment.appendChild(btnDismiss);
 
             // 整合进蒙层
             overlay.appendChild(fragment);
