0f160002debe0689159d351689b272de2a2dd75e
yuhao.ju <yuhao.ju@qunar.com>
2017-11-01 16:34:33 +0800

    fix: 去掉 hot-reload 对于 publicPath 的修改

--- src/commands/server.js
+++ src/commands/server.js
@@ -233,21 +233,14 @@ exports.run = (options) => {
         // hot reload
         const hotEnabled = (project.server && project.server.hot) || hot;
         if(hotEnabled) {
-            // 修改 publicPath 为当前服务
-            let localPublicPath = wpConfig.output.local.publicPath;
-            if(project.config.replacingPublicPath !== false) {
+            const localPublicPath = wpConfig.output.local.publicPath;
+            if (project.config.replacingPublicPath !== false) {
                 const hostReg = /(http:|https:)?(\/\/)([^\/]+)/i;
-                if(localPublicPath && localPublicPath.match(hostReg).length === 4) {
-                    localPublicPath = '/' + UtilPath.normalize(localPublicPath, false);
-                    localPublicPath = localPublicPath.replace(hostReg, (matches, httpStr, splitStr, host) => {
-                        httpStr = httpStr || '';
-                        return httpStr + '//' + '127.0.0.1:' + port;
-                    });
-                    wpConfig.output.local.publicPath = localPublicPath;
-                } else {
+
+                if (!localPublicPath) {
                     // hot 且 未指定 publicPath 需要手动设置方式 hot.json 404
                     const relativePath = sysPath.relative(projectDir, wpConfig.output.local.path);
-                    wpConfig.output.local.publicPath = `http://127.0.0.1:${port}/${projectName}/${relativePath}/`;
+                    wpConfig.output.local.publicPath = 'http://127.0.0.1:' + port + '/' + projectName + '/' + relativePath + '/';
                 }
             }
 
