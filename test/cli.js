d9e40c9bbe12fed115b458ca3d828a3d77af6bf1
yuhao.ju <yuhao.ju@qunar.com>
2017-11-24 19:43:01 +0800

    test: 修复 circleci 测试

--- test/cli.js
+++ test/cli.js
@@ -86,7 +86,7 @@ describe('Start testing terminal client', () => {
         shell.cd(path.join(examplePath, 'node_modules'));
         kill();
 
-        const child = shell.exec(`${ykitBin} server -p ${port}`, {silent: false}, () => {
+        const child = shell.exec(`${ykitBin} server -p ${port + 1}`, {silent: false}, () => {
             // do nothing & wait for curl
         });
 
@@ -98,7 +98,7 @@ describe('Start testing terminal client', () => {
             }
             hasTestCurl = true;
 
-            const output = shell.exec(`curl -I localhost:${port}/react/package.json`, {silent: false});
+            const output = shell.exec(`curl -I localhost:${port + 1}/react/package.json`, {silent: false});
             expect(output.includes('200')).to.be.true;
 
             kill();
