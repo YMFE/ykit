898cd4f2159f3639310ff6b80e3f8f3fb4dd73f3
yuhao.ju <yuhao.ju@qunar.com>
2017-11-27 14:29:43 +0800

    feat: 添加 stat 命令

--- src/commands/stat.js
+++ src/commands/stat.js
@@ -4,16 +4,18 @@ const shell = require('shelljs');
 const packCMD = require('./pack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-exports.usage = '分析项目 bundle';
+exports.usage = '打包结果分析';
 
 exports.setOptions = () => {
 
 };
 
 exports.run = function(options) {
-	console.log('stat!');
 	let config = this.project.config.getConfig();
 	config.plugins.push(new BundleAnalyzerPlugin(options || {}));
 
-	packCMD.run.bind(this)({})
+	packCMD.run.bind(this)({
+		quiet: true,
+		sourcemap: false
+	});
 };
