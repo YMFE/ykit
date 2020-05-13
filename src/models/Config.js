80c35d02096005689d11bdfb797553cf2e9be51c
Mozi <qitmac@MozideMacBook-Pro.local>
2020-05-13 11:04:25 +0800

    init

--- src/models/Config.js
+++ src/models/Config.js
@@ -1,8 +1,6 @@
 'use strict';
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin") ;
-
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const normalize = require('../utils/path').normalize;
